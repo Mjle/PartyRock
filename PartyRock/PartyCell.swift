e9d382f87e49a64a96f0309f3f02d66908fab847
Michael Le <mjle@ucsc.edu>
2017-04-24 22:40:13 -0700

    first commit

---
+++ PartyRock/PartyCell.swift
@@ -0,0 +1,24 @@
+//
+//  PartyCell.swift
+//  PartyRock
+//
+//  Created by Michael Le on 4/16/17.
+//  Copyright © 2017 Michael Le. All rights reserved.
+//
+
+import UIKit
+
+class PartyCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ PartyRock/PartyRock.swift
@@ -0,0 +1,9 @@
+//
+//  PartyRock.swift
+//  PartyRock
+//
+//  Created by Michael Le on 4/16/17.
+//  Copyright © 2017 Michael Le. All rights reserved.
+//
+
+import Foundation
