78438b924748576d01d461f44c610acff174cca6
Michael Le <mjle@ucsc.edu>
2017-04-24 22:40:32 -0700

    all files

--- PartyRock/PartyCell.swift
+++ PartyRock/PartyCell.swift
@@ -10,15 +10,27 @@ import UIKit
 
 class PartyCell: UITableViewCell {
 
+    @IBOutlet weak var videoTitle: UILabel!
+    @IBOutlet weak var videoPreviewImage: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func updateUI(partyRock: PartyRock) {
+        videoTitle.text = partyRock.videoTitle
+        
+        let url = URL(string: partyRock.imageURL)!
+        DispatchQueue.global().async {
+            do {
+                let data = try Data(contentsOf: url)
+                DispatchQueue.global().sync {
+                    self.videoPreviewImage.image = UIImage(data: data)
+                }
+            } catch {
+                // handle error
+            }
+        }
     }
-
 }
